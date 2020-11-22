f4368fa2862d19e3a68201d6f60896725e6f6ba0
Sathiya Lingam <slsathiya20@gmail.com>
2020-11-22 12:59:13 +0000

    The section-4 challenge program

---
+++ Sec_4/34_SecChallenge/sol.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void){
+
+  int num;
+  std::cout << "Enter your favorite number:";
+  std::cin>>num;
+  std::cout<< "Amazing!!! That's my favorite number too!"<<std::endl;
+  std::cout<<"No really!!, "<< num << " is my favorite number!"<<std::endl;
+
+  return 0;
+}
